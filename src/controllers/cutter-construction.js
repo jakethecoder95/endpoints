58f726e978d79eeb75aaed1560647ba0d585ae73
Jacob Robinson <95jacob07@gmail.com>
2019-06-25 09:29:15 -0700

    added .env

--- src/controllers/cutter-construction.js
+++ src/controllers/cutter-construction.js
@@ -1,17 +1,17 @@
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
-
-// const { SENDGRID_KEY } = require("../secrets");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key: process.env.SENDGRID_KEY /* || SENDGRID_KEY */
+      api_key: process.env.SENDGRID_KEY
     }
   })
 );
 
 exports.cutterConstruction = async (req, res, next) => {
+  console.log(process.env.SENDGRID_KEY);
   const { name, email, message, subject, phone } = req.body;
   try {
     const response = await transporter.sendMail({
@@ -26,7 +26,7 @@ exports.cutterConstruction = async (req, res, next) => {
     });
     res.status(200).json({
       msg: "success",
-      response
+      response: `${process.env.SENDGRID_KEY}`
     });
   } catch (error) {
     if (!error.statusCode) {
